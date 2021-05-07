5f73cd9d91ff3e48cb8cd6fb5e5b09ea748c0074
brendamarcos <brendammarcoss22@gmail.com>
2021-05-07 01:52:46 -0500

    Update

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -1,7 +1,9 @@
 import request from "request";
 
-let getFacebookUserName = (sender_psid) => {
-    return new Promise ((resolve, reject) => {
+require("dotenv").config();
+
+let getFacebookUsername = (sender_psid) => {
+    return new Promise((resolve, reject) => {
         // Send the HTTP request to the Messenger Platform
         let uri = `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`;
         request({
@@ -9,14 +11,15 @@ let getFacebookUserName = (sender_psid) => {
             "method": "GET",
         }, (err, res, body) => {
             if (!err) {
+                //convert string to json object
                 body = JSON.parse(body);
                 let username = `${body.last_name} ${body.first_name}`;
                 resolve(username);
             } else {
                 reject("Unable to send message:" + err);
             }
-        }); 
     });
+});
 };
 
 module.exports = {
